d9fa3752c5c93b42dbd5719ed904271d69af598b
sarahjholm <sarahjholm@gmail.com>
2024-06-24 18:42:10 +0100

    Altered date column in CurrencyRate table to be unique

--- currency_app/models.py
+++ currency_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class CurrencyRate(models.Model) :
-    date = models.DateTimeField()
+    date = models.DateTimeField(unique=True)
     value = models.FloatField(max_length=6)
